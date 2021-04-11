09801ff5bb9614674bb7e9181a05d5750455d1e3
doldsimo <simon.dold@hs-furtwangen.de>
2021-04-11 20:42:48 +0200

    TAKS: legend

--- client/src/components/CardLegend/CardLegend.js
+++ client/src/components/CardLegend/CardLegend.js
@@ -1,20 +1,18 @@
-import React, { useState } from 'react';
-import { IonModal, IonButton, IonContent, IonFab, IonFabButton, IonIcon, IonFabList, IonText } from '@ionic/react';
-import { informationCircleOutline, add, settings, share, person, arrowForwardCircle, arrowBackCircle, arrowUpCircle, logoVimeo, logoFacebook, logoInstagram, logoTwitter } from 'ionicons/icons';
-import { Popup } from 'react-leaflet';
+import React from 'react';
+import { IonButton, IonFab, IonFabButton, IonIcon, IonFabList, } from '@ionic/react';
+import { informationCircleOutline } from 'ionicons/icons';
 
-const CardLegend = ({ isLegendOpen, setIsLegendOpen }) => {
+const CardLegend = () => {
     return (
-
         <IonFab horizontal="end" vertical="top" slot="fixed"  >
             <IonFabButton size="small" color="light" >
                 <IonIcon icon={informationCircleOutline} />
             </IonFabButton>
             <IonFabList side="bottom" >
-                <IonButton size="small" color="light"><IonIcon icon={logoVimeo} /></IonButton>
-                <IonButton size="small" color="light"><IonIcon icon={logoFacebook} /></IonButton>
-                <IonButton size="small" color="light"><IonIcon icon={logoInstagram} /></IonButton>
-                <IonButton size="small" color="light"><IonIcon icon={logoTwitter} /></IonButton>
+                <IonButton size="small" color="light"><IonIcon icon={informationCircleOutline} /></IonButton>
+                <IonButton size="small" color="light"><IonIcon icon={informationCircleOutline} /></IonButton>
+                <IonButton size="small" color="light"><IonIcon icon={informationCircleOutline} /></IonButton>
+                <IonButton size="small" color="light"><IonIcon icon={informationCircleOutline} /></IonButton>
             </IonFabList>
         </IonFab>
     )
