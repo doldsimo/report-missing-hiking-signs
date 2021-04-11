044ec63b6814787f0474a6946a764b395606728a
doldsimo <simon.dold@hs-furtwangen.de>
2021-04-11 20:36:21 +0200

    TASK: adding difference map marker

---
+++ client/src/components/MapMarker/MapMarker.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import L from "leaflet";
+import redMarker from '../../assets/mapMarker/marker-icon-red.png';
+import blueMarker from '../../assets/mapMarker/marker-icon-blue.png';
+import markerShadow from '../../assets/mapMarker/marker-shadow.png';
+
+export function RedMarker() {
+    return L.icon({
+        iconUrl: redMarker,
+        shadowUrl: markerShadow,
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+}
+
+export function BlueMarker() {
+    return L.icon({
+        iconUrl: blueMarker,
+        shadowUrl: markerShadow,
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+}
+
