2908b440a73c2b811fbda563a4fc54b6a8c7faae
doldsimo <simon.dold@hs-furtwangen.de>
2021-05-06 22:09:11 +0200

    TASK: fix marker onclick

--- client/src/components/GeolocationView/GeolocationView.js
+++ client/src/components/GeolocationView/GeolocationView.js
@@ -1,18 +1,17 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { MapContainer, Popup, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-// import Marker from 'react-leaflet-enhanced-marker';
 import { RedMarker, BlueMarker } from '../MapMarker/MapMarker';
 
 
-import { IonText, IonButton, IonItem, IonLabel, useIonViewDidEnter } from '@ionic/react';
+import { useIonViewDidEnter, IonIcon, IonFab, IonFabButton } from '@ionic/react';
 import CardLegend from './CardLegend/CardLegend';
 import LocationService from '../../hooks/useGeolocationService';
 import { AlertContext } from '../../context/AlertContext';
 import { LocationsContext } from '../../context/LocationsContext';
 import OtherLocationsModal from './OtherLocationsModal/OtherLocationsModal';
 import './styles.css';
-
+import { locationOutline } from 'ionicons/icons';
 
 const GeolocationView = () => {
     const [map, setMap] = useState(null);
@@ -23,14 +22,16 @@ const GeolocationView = () => {
     const { setAlertMessage } = useContext(AlertContext);
 
     const handleOnFlyTo = async () => {
-        let coordinates = await LocationService.getCurrentLocation();
+        let coordinatesData = await LocationService.getCurrentLocation();
 
-        // For Error Toast Messages
-        if (typeof coordinates === 'string') {
-            setAlertMessage(coordinates)
+        let coordinates = [coordinatesData.coords.latitude, coordinatesData.coords.longitude];
+
+        // For error toast messages
+        if (typeof coordinatesData === 'string') {
+            setAlertMessage(coordinatesData)
         } else {
-            setUserLocation([coordinates.coords.latitude, coordinates.coords.longitude]);
-            map.flyTo([coordinates.coords.latitude, coordinates.coords.longitude]);
+            setUserLocation(coordinates);
+            map.flyTo(coordinates);
         }
     }
 
@@ -42,6 +43,7 @@ const GeolocationView = () => {
     useIonViewDidEnter(() => {
         window.dispatchEvent(new Event('resize'));
     });
+
     return (
         <>
             <MapContainer center={userLocation} zoom={13} style={{ height: "100%", width: "100%" }} whenCreated={setMap}>
@@ -53,36 +55,32 @@ const GeolocationView = () => {
 
                 {locations.map((location, i) => {
                     return (
-                        <Marker key={i} position={location.coordinates} icon={BlueMarker()} detectRetina>
-                            <Popup>
-                                Gemeldete Position
-                                <div onClick={() => {
+                        <Marker
+                            key={i}
+                            position={location.coordinates}
+                            icon={BlueMarker()}
+                            eventHandlers={{
+                                click: () => {
                                     setIsModalOpen(true);
                                     setLocationId(location._id);
-                                }}
-                                    style={{ color: "#0044CC", cursor: "pointer" }}
-                                >
-                                    mehr...
-                                </div>
-                            </Popup>
+                                },
+                            }}>
                         </Marker>
                     );
                 })}
 
-                <Marker position={userLocation} icon={RedMarker()} zIndexOffset={100} detectRetina>
+                <Marker position={userLocation} icon={RedMarker()} zIndexOffset={100} >
                     <Popup>
                         Dein Standort
                     </Popup>
                 </Marker>
             </MapContainer>
 
-            <IonItem className='geoAbs'>
-                <IonLabel>
-                    <IonText>lat={userLocation[0]}</IonText>
-                    <IonText> lng={userLocation[1]}</IonText>
-                </IonLabel>
-            </IonItem>
-            <IonButton onClick={handleOnFlyTo} className='geoFooter'>Get Current Location</IonButton>
+            <IonFab vertical="bottom" horizontal="center" slot="fixed">
+                <IonFabButton onClick={handleOnFlyTo} style={{ opacity: 0.7 }}>
+                    <IonIcon icon={locationOutline} />
+                </IonFabButton>
+            </IonFab>
             <CardLegend isLegendOpen={isLegendOpen} setIsLegendOpen={setIsLegendOpen} />
             { locationId != undefined && <OtherLocationsModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} locationId={locationId} setLocationId={setLocationId} />}
         </>
