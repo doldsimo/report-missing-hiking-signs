f2961614e9280d5a243bb20feb278654563a7390
doldsimo <simon.dold@hs-furtwangen.de>
2021-05-11 18:01:15 +0200

    TASK: update api base url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+// const API = axios.create({ baseURL: 'http://localhost:5000' });
+
+// if backend is running in the same network
+// const API = axios.create({ baseURL: 'http://yourIP:5000' });
+const API = axios.create({ baseURL: 'http://192.168.2.113:5000' });
 
 
 export const fetchReportPostsCoordinates = () => API.get('/reportPosts');
